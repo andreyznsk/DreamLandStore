18a73207dff36d1934c9b34fb5ba497d58253172
andreyznsk <andreyznsk@gmail.com>
2021-05-20 22:52:13 +0700

    initial commit

---
+++ src/main/java/ru/geekbrains/DreamLandStore/DreamLandStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.DreamLandStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamLandStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamLandStoreApplication.class, args);
+	}
+
+}
